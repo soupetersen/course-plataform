2a5fa542aa66a735b8afa52054eafb53e4a926ce
Thiago Petersen <soupetersen@gmail.com>
2025-06-21 14:08:28 -0300

    feat: wrap RouterProvider with ErrorProvider and add Toaster component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,18 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./lib/query-client";
 import { router } from "./routes";
+import { ErrorProvider } from "./contexts/ErrorContext";
+import { Toaster } from "./components/ui/toaster";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ErrorProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ErrorProvider>
     </QueryClientProvider>
   </StrictMode>
 );
